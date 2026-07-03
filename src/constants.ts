6790661daa0f4aba28e37eb7b37b4ad7259ef1ef
ingridleriksen <ingrid.l.eriksen@knowit.no>
2026-07-03 10:39:29 +0200

    Add Pickcode to external whitelist providers and content security policy

--- src/constants.ts
+++ src/constants.ts
@@ -228,6 +228,7 @@ export const EXTERNAL_WHITELIST_PROVIDERS: WhitelistProvider[] = [
   { name: "Ebok", url: ["ebok.no"] },
   { name: "VG", url: ["www.vg.no"] },
   { name: "Trinket", url: ["trinket.io"], height: "700px" },
+  { name: "Pickcode", url: ["pickcode.io"], height: "700px" },
   { name: "Codepen", url: ["codepen.io"], height: "500px" },
   {
     name: "Flourish studio",
