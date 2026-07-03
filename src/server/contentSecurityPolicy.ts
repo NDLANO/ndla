1f25eb00e8b8e05d275350407c8873389f65e1ce
ingridleriksen <ingrid.l.eriksen@knowit.no>
2026-07-03 12:09:12 +0200

    update pickcode.io entry

--- src/server/contentSecurityPolicy.ts
+++ src/server/contentSecurityPolicy.ts
@@ -185,7 +185,7 @@ const frameSrc = (() => {
     "reader.pubfront.com",
     "ebok.no",
     "trinket.io",
-    "pickcode.io",
+    "*.pickcode.io",
     "codepen.io",
     "public.flourish.studio",
     "flo.uri.sh",
