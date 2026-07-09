302c24edafefcbe34f19c562017f01693e387c67
Jonas Natten <jonas.natten@knowit.no>
2026-07-09 22:04:29 +0200

    refactor(ci): frontend CI to utilize `nx affected`
    This patch improves CI runs on the frontend by improving the nx cache as
    well as utilizing `nx affected` to determine what needs running.
    
    This should prevent us from running unnecessary CI jobs on the frontend.

--- frontend/editorial-frontend/playwright.config.ts
+++ frontend/editorial-frontend/playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   retries: 2,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.RECORD_FIXTURES ? 1 : process.env.CI ? 10 : undefined,
   reporter: "html",
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
