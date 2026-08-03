60767e76cde613c42c998f8f54065dd33887ab15
Jonas Natten <jonas.natten@knowit.no>
2026-08-03 08:52:33 +0200

    refactor(frontend): clean up watch commands and panda HMR

--- frontend/editorial-frontend/panda.config.ts
+++ frontend/editorial-frontend/panda.config.ts
@@ -9,8 +9,6 @@
 import preset from "@ndla/preset-panda";
 import { defineConfig } from "@pandacss/dev";
 
-const isDev = process.env.NODE_ENV !== "production";
-
 export default defineConfig({
   presets: [preset],
   preflight: true,
@@ -20,9 +18,8 @@ export default defineConfig({
   outExtension: "js",
   include: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    ...(isDev
-      ? ["../packages/packages/*/src/**/*.{js,jsx,ts,tsx}"]
-      : ["./node_modules/@ndla/*/dist/panda.buildinfo.json", "../node_modules/@ndla/*/dist/panda.buildinfo.json"]),
+    "./node_modules/@ndla/*/dist/panda.buildinfo.json",
+    "../node_modules/@ndla/*/dist/panda.buildinfo.json",
   ],
   syntax: "object-literal",
   jsxFramework: "react",
