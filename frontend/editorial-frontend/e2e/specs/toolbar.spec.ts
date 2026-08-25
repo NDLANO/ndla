f453e88ab2809f701508d95fdad89dcf11fa0a9e
Gunnar Velle <gunnar.velle@gmail.com>
2026-08-25 10:23:26 +0200

    Use normalized url in test

--- frontend/editorial-frontend/e2e/specs/toolbar.spec.ts
+++ frontend/editorial-frontend/e2e/specs/toolbar.spec.ts
@@ -94,8 +94,8 @@ test("can create a valid link", async ({ page }) => {
   await page.locator('input[name="href"]').fill("http://www.vg.no");
   await page.getByText("Sett inn lenke").click();
   await expect(page.getByText("Legg til lenke")).toHaveCount(0);
-  await expect(page.locator('a[href="http://www.vg.no"][data-slate-node="element"]')).toBeVisible();
-  await expect(page.locator('a[href="http://www.vg.no"][data-slate-node="element"]')).toHaveText("This is a test link");
+  await expect(page.locator('a[href="http://www.vg.no/"][data-slate-node="element"]')).toBeVisible();
+  await expect(page.locator('a[href="http://www.vg.no/"][data-slate-node="element"]')).toHaveText("This is a test link");
 });
 
 test("All lists work properly", async ({ page }) => {
