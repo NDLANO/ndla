544cd0b0a4ed51b8e32d0019f389b3b9d8ad455b
ingridleriksen <ingrid.l.eriksen@knowit.no>
2026-08-26 15:07:20 +0200

    save and share quiz page

--- frontend/ndla-frontend/src/appRoutes.tsx
+++ frontend/ndla-frontend/src/appRoutes.tsx
@@ -257,6 +257,11 @@ export const routes: NdlaRouteObject[] = [
                 importPath: "src/containers/MyNdla/Quiz/ReviewQuizPage.tsx",
                 lazy: () => import("./containers/MyNdla/Quiz/ReviewQuizPage"),
               },
+              {
+                path: ":quizId/save",
+                importPath: "src/containers/MyNdla/Quiz/SaveQuizPage.tsx",
+                lazy: () => import("./containers/MyNdla/Quiz/SaveQuizPage"),
+              },
             ],
           },
           {
