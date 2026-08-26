85a7a537798219f0466e01d553ff8fc6b1a15221
Amandus Søve Thorsrud <amandus.thorsrud@knowit.no>
2026-08-26 14:21:47 +0200

    frontend: Clean up custom `.d.ts` files

---
+++ frontend/editorial-frontend/src/types/htmldiff-js.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2026-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+declare module "htmldiff-js" {
+  export default class HtmlDiff {
+    static execute(oldHtml: string, newHtml: string): string;
+  }
+}
