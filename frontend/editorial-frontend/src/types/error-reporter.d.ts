c201d8485d4d1be6aa9689cc3fdc9062ed0f69d3
Amandus Søve Thorsrud <amandus.thorsrud@knowit.no>
2026-08-27 10:00:29 +0200

    frontend: Clean up custom `.d.ts` files
    Removes the catch-all global type declarations in ndla-frontend and
    editorial-frontend, and instead uses typing packages where available.
    
    Reviewers: jnatten, #frontenders
    
    Reviewed By: jnatten
    
    Pull Request: https://github.com/NDLANO/ndla/pull/84

--- frontend/editorial-frontend/src/types/error-reporter.d.ts
+++ frontend/editorial-frontend/src/types/error-reporter.d.ts
@@ -1,7 +1,11 @@
-/*
- * Copyright (c) 2019-present, NDLA.
+/**
+ * Copyright (c) 2026-present, NDLA.
+ *
  * This source code is licensed under the GPLv3 license found in the
  * LICENSE file in the root directory of this source tree.
+ *
  */
 
-declare module "*";
+declare module "@ndla/error-reporter" {
+  export const ErrorReporter: any;
+}
