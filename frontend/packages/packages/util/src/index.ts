266ed454669226f1467367e9db941c132d7b1660
Jonas Natten <jonas.natten@knowit.no>
2026-08-31 23:34:33 +0200

    refactor(util): share the Feide cookie names

--- frontend/packages/packages/util/src/index.ts
+++ frontend/packages/packages/util/src/index.ts
@@ -7,6 +7,13 @@
  */
 
 export { setCookie, getCookie, deleteCookie, isValidCookie } from "./cookieHandler";
+export {
+  FEIDE_ID_TOKEN_COOKIE,
+  FEIDE_NONCE_COOKIE,
+  FEIDE_PKCE_CODE_COOKIE,
+  FEIDE_RETURN_TO_COOKIE,
+  FEIDE_STATE_COOKIE,
+} from "./feideCookies";
 export { printPage } from "./printPage";
 export { validateTranslationFiles } from "./translationValidation";
 export { NoSSR } from "./nossr/NoSSR";
