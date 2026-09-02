c4583462923a5aebde1c19ed6383d8dcfc280b21
ingridleriksen <ingrid.l.eriksen@knowit.no>
2026-09-02 13:07:33 +0200

    question has correct answer alert

--- frontend/ndla-frontend/src/messages/messagesNN.ts
+++ frontend/ndla-frontend/src/messages/messagesNN.ts
@@ -460,6 +460,7 @@ const messages = {
         questionNumber: "Spørsmål {{number}}",
         questionTitle: "Spørsmålstekst",
         questionTitlePlaceholder: "Skriv spørsmålet ditt her",
+        noCorrectAnswer: "Du må velje eit rett svar",
         cardTitle: "Kviss",
         renameQuiz: "Endre namn",
         navigation: "Kvissnavigasjon",
