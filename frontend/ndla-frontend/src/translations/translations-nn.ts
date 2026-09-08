f61bad79103b9f0370a4c215985fec6ee79b447e
ingridleriksen <ingrid.l.eriksen@knowit.no>
2026-09-08 14:18:32 +0200

    remove yarn files + typo

--- frontend/ndla-frontend/src/translations/translations-nn.ts
+++ frontend/ndla-frontend/src/translations/translations-nn.ts
@@ -38,7 +38,7 @@ const translations = {
     learningpathPreviewPage: `Førehandsvis - {{name}} - Læringssti - ${titleTemplate}`,
     learningpathSavePage: `Lagre - {{name}} - Læringssti - ${titleTemplate}`,
     learningpathNewPage: `Ny læringssti - ${titleTemplate}`,
-    quizPage: `Mine kvissar - ${titleTemplate}`,
+    quizPage: `Mine kvisser - ${titleTemplate}`,
     quizNewPage: `Ny kviss - ${titleTemplate}`,
     quizEditPage: `Rediger kviss - ${titleTemplate}`,
     quizSavePage: `Lagre og del kviss - ${titleTemplate}`,
@@ -409,7 +409,7 @@ const translations = {
       dragHandle: "Sorter svaralternativ {{name}}",
     },
     quiz: {
-      title: "Mine kvissar",
+      title: "Mine kvisser",
       description: "Her kan du opprette dine eigne kvisser og dele dei med elevane dine.",
       newQuiz: "Opprett ny kviss",
       noQuiz: "Du har ikkje laga nokon kviss enno.",
