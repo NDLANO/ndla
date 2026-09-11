56299ec8c8fea5df3df31fe32a3ebd516adce17a
Gunnar Velle <gunnar.velle@gmail.com>
2026-09-11 13:48:35 +0200

    Use status enums for path params also

--- frontend/editorial-frontend/src/containers/EditSubjectFrontpage/components/SubjectpageArticles.tsx
+++ frontend/editorial-frontend/src/containers/EditSubjectFrontpage/components/SubjectpageArticles.tsx
@@ -80,7 +80,9 @@ const SubjectpageArticles = ({ editorsChoices, elementId, fieldName }: Props) =>
       } else {
         const f = result.learningResourceType === "learningpath" ? fetchLearningpath : fetchDraft;
         const newResource = await f(result.id);
-        const newResources = resources.concat({ ...newResource }); // , metaImage: result.metaImage // does not exist for learningpath
+        const newResources = resources.concat(
+          "metaImage" in newResource ? newResource : { ...newResource, metaImage: result.metaImage },
+        );
         setResources(newResources);
         updateFormik(newResources);
       }
