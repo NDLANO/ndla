6fc164e7d8a946b240a49f44432317b43951d7fa
Jonas Natten <jonas.natten@knowit.no>
2026-09-14 13:58:38 +0200

    refactor(frontend): inline @ndla/tracker into ndla-frontend (#141)

--- frontend/ndla-frontend/src/components/Scripts/Tagmanager.tsx
+++ frontend/ndla-frontend/src/components/Scripts/Tagmanager.tsx
@@ -6,8 +6,26 @@
  *
  */
 
-import { MatomoTagManager } from "@ndla/tracker";
 import config from "../../config";
+
+interface Props {
+  trackerUrl: string;
+  containerId: string;
+}
+
+const MatomoTagManager = ({ trackerUrl, containerId }: Props) => {
+  return (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+        var _mtm = window._mtm = window._mtm || [];
+        _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src='${trackerUrl}/js/${containerId}.js'; s.parentNode.insertBefore(g,s);`,
+      }}
+    />
+  );
+};
 
 export const Tagmanager = () => {
   if (config.matomoTagmanagerId) {
